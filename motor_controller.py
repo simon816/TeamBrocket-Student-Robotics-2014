d809fddbcfd2248c0387324ac377f6042db60b39
simon816 <simonbarnes1@googlemail.com>
2015-04-26 20:21:03 +0100

    Relicense under the MIT License

--- motor_controller.py
+++ motor_controller.py
@@ -1,18 +1,6 @@
 """
-    This file is part of Team Brocket Robotics (herein BrocketSource).
-
-    BrocketSource is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    BrocketSource is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with BrocketSource. If not, see <http://www.gnu.org/licenses/>.
+    This file is part of Team Brocket Robotics, licensed under the MIT License.
+    A copy of the MIT License can be found in LICENSE.txt
 """
 
 import math
